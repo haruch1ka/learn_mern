701936323d5dfeeed9ec1ee4b2e54c005aa6007b
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-01-09 10:46:53 +0900

    Expressサーバーのセットアップとルーティングの追加

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+//endpoint
+const userRoute = require("./router/users");
+const authsRoute = require("./router/auth");
+const postsRoute = require("./router/posts");
+
+//middleware
+app.use("/api/users", userRoute);
+app.use("/api/auth", authsRoute);
+app.use("/api/posts", postsRoute);
+
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
