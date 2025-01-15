cbd4bf1b7f59bc01a137b3f5e6fdc820351bca74
d-ito <sican1868@gmail.com>
2025-01-15 22:24:41 +0900

    ユーザ情報の削除機能と取得機能の追加

--- router/users.js
+++ router/users.js
@@ -20,6 +20,29 @@ router.put("/:id", async (req, res) => {
 });
 
 //ユーザ情報の削除
+
+router.delete("/:id", async (req, res) => {
+	if (req.body.userId == req.params.id || req.body.isAdmin) {
+		try {
+			const user = await User.findByIdAndDelete(req.params.id);
+			return res.status(200).json("削除できた");
+		} catch (error) {
+			return res.status(500).json(error);
+		}
+	} else {
+		return res.status(403).json("自分の情報だけ削除できる");
+	}
+});
 //ユーザ情報の取得
+
+router.get("/:id", async (req, res) => {
+	try {
+		const user = await User.findByIdAndDelete(req.params.id);
+		const { password, updatedAt, ...other } = user._doc;
+		return res.status(200).json(other);
+	} catch (error) {
+		return res.status(500).json(error);
+	}
+});
 
 module.exports = router;
