0197da76591d76346a35334971347529ea821c5f
d-ito <sican1868@gmail.com>
2025-01-16 22:14:55 +0900

    スタイルシートの追加、サイドバーのインポート、トップバーのスタイルと機能を実装

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,10 @@
 import Topbar from "./../components/topbar/Topbar";
+import Sidebar from "./../components/sidebar/Sidebar";
 const Home = () => {
 	return (
 		<>
 			<Topbar />
+			<Sidebar />
 		</>
 	);
 };
