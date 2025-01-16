e87a018d4853ecf8a028e98cc993aedcd19ecc9b
d-ito <sican1868@gmail.com>
2025-01-16 22:17:23 +0900

    タイムラインコンポーネントとスタイルシートを追加し、ホームページに統合

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import Topbar from "./../components/topbar/Topbar";
 import Sidebar from "./../components/sidebar/Sidebar";
+import Timeline from "./../components/timeline/Timeline";
+import Rightbar from "./../components/rightbar/Rightbar";
 const Home = () => {
 	return (
 		<>
 			<Topbar />
 			<Sidebar />
+			<Timeline />
+			<Rightbar />
 		</>
 	);
 };
