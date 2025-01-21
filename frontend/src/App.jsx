cc212945bbae2f0f442382891b5b6f2f1f7d5584
d-ito <sican1868@gmail.com>
2025-01-21 23:24:20 +0900

    ユーザー認証に基づいてルーティングを更新。ログイン済みユーザーはホームにリダイレクトされ、未ログインユーザーは登録ページに誘導されるように変更。

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,20 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Profile from "./pages/profile/Profile";
+import { AuthContext } from "./state/AuthContext";
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
+	const { user } = useContext(AuthContext);
 	return (
 		<>
 			<Router>
 				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/register" element={<Register />} />
+					<Route path="/" element={user ? <Home /> : <Register />} />
+					<Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+					<Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
 					<Route path="/profile/:username" element={<Profile />} />
 				</Routes>
 			</Router>
