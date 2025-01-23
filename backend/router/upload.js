d42cc545354008e78c45cd7809a6345dda66e643
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-01-23 12:45:20 +0900

    multerを使用してファイルアップロードのルートを実装し、アップロード成功時のレスポンスを追加

--- backend/router/upload.js
+++ backend/router/upload.js
@@ -1,6 +1,25 @@
 const router = require("express").Router();
 const multer = require("multer");
 
+//multerの設定
 
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "public/images");
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.originalname);
+	},
+});
+
+const upload = multer({ storage });
+
+router.post("/", upload.single("file"), (req, res) => {
+	try {
+		return res.status(200).json("File uploaded successfully");
+	} catch (error) {
+		console.log(error);
+	}
+});
 
 module.exports = router;
